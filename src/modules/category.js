279f310618d51fab5b69256e06acaed0619967ad
RjRaju143 <bangarraju1152@gmail.com>
2023-08-29 14:37:33 +0530

    category

---
+++ src/modules/category.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+});
+
+//// Define a virtual property for the ID
+categorySchema.virtual("id").get(function() {
+  return this._id.toHexString();
+});
+
+///// Ensure virtuals are included when converting to JSON
+categorySchema.set("toJSON", { virtuals: true });
+
+//// Create and export the Category model
+export const Category = mongoose.model("Category", categorySchema);
+
