6463180bb2feb1a8e1fa9e2d807bf6065b2c3cb9
RjRaju143 <bangarraju1152@gmail.com>
2023-08-29 14:37:33 +0530

    product

---
+++ src/modules/product.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose"
+
+const productsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  brand: {
+    type: String,
+    default: "",
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  newReviews: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+productsSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+productsSchema.set("toJSON", { virtuals: true });
+
+export const Product = mongoose.model("Product", productsSchema);
+
