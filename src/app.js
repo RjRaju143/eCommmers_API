755eb353f02c7739f885194557df43237c53ea57
RjRaju143 <bangarraju1152@gmail.com>
2023-09-04 20:25:32 +0530

    some changes

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const staticPath = path.join(__dirname, "public/uploads");
+const staticPath = path.join(__dirname, "/public/uploads");
 app.use("/public/uploads", express.static(staticPath));
 
 /// jwt handler
